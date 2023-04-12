629f19f50eccf855fce8d76fb2ca69cb8220f004
Sohee Kim <74011724+elbica@users.noreply.github.com>
2023-04-12 20:52:00 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 3.63 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -5,7 +5,7 @@
 #include<algorithm>
 
 using namespace std;
-int visited[30][30];
+int visited[30][30][4];
 int dy[4] = {0, 1, 0, -1};
 int dx[4] = {1, 0, -1, 0};
 
@@ -20,21 +20,21 @@ struct pos {
 int solution(vector<vector<int>> board) {
     int answer = 0, size = board.size();
     
-    priority_queue<pos> q;
+    queue<pos> q;
     q.push({0, 0, 0 , -1, -1});
-    fill(&visited[0][0], &visited[29][30], 987654321);
-    visited[0][0] = 0;
+    fill(&visited[0][0][0], &visited[29][29][4], 987654321);
+    visited[0][0][0] =visited[0][0][1] = visited[0][0][2] = visited[0][0][3]=0;
     
     while(!q.empty()){
-        auto [cost, y, x, prevY, prevX] = q.top(); q.pop();
+        auto [cost, y, x, prevY, prevX] = q.front(); q.pop();
         for(int i=0; i<4; i++){
             int nextY = y + dy[i], nextX = x+dx[i];
             if(nextY < 0 || nextY >= size || nextX < 0 || nextX >= size) continue;
             if(board[nextY][nextX]) continue;
             int money = (prevY == nextY || prevX == nextX || prevX == -1) ? 100 : 600;
             
-            if(visited[nextY][nextX] < cost + money) continue;
-            visited[nextY][nextX] = cost+money;
+            if(visited[nextY][nextX][i] < cost + money) continue;
+            visited[nextY][nextX][i] = cost+money;
             // cout<<cost + money<<' '<<nextY<<' '<<nextX<<' '<<y<<' '<<x<<' '<<prevY<<' '<<prevX<<' '<<money<<endl;
             
             q.push({cost+money, nextY, nextX, y, x});
@@ -43,5 +43,5 @@ int solution(vector<vector<int>> board) {
     }
     
 
-    return visited[size-1][size-1];
+    return *min_element(&visited[size-1][size-1][0], &visited[size-1][size-1][4]);
 }
